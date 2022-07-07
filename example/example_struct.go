d2b24b40d9022661cca65174d164030f61fb9c3c
ricdeau <ricdeau1@gmail.com>
2022-07-07 20:39:29 +0300

    array, slice, map, chans (#1)
    Co-authored-by: Andrey Shmelyov <ricdeau@yandex.ru>

--- example/example_struct.go
+++ example/example_struct.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	js "encoding/json"
 	"io"
+	"os"
 	"time"
 )
 
@@ -15,6 +16,12 @@ type ExampleStruct struct {
 	Base    base64.CorruptInputError
 	Time    time.Time
 	Iface   io.ReadCloser
+	Array   [3]int
+	Slice   []*os.File
+	Map     map[io.ByteWriter]string
+	inCh    chan<- string
+	outCh   <-chan int
+	ch      chan *float64
 	private int
 }
 
@@ -40,4 +47,28 @@ func (e *ExampleStruct) GetIface() io.ReadCloser {
 
 func (e *ExampleStruct) GetPrivate() int {
 	return e.private
+}
+
+func (e *ExampleStruct) GetArray() [3]int {
+	return e.Array
+}
+
+func (e *ExampleStruct) GetSlice() []*os.File {
+	return e.Slice
+}
+
+func (e *ExampleStruct) GetMap() map[io.ByteWriter]string {
+	return e.Map
+}
+
+func (e *ExampleStruct) GetInCh() chan<- string {
+	return e.inCh
+}
+
+func (e *ExampleStruct) GetOutCh() <-chan int {
+	return e.outCh
+}
+
+func (e *ExampleStruct) GetCh() chan *float64 {
+	return e.ch
 }
